d686bec25d17005514c2f96503345e2892a6be1e
boardude <yzhu13@outlook.com>
2021-02-10 12:43:46 +1000

    Remove close button from modals
    Users can click anywhere outside the modal box to close the modal. However, may need revision for accessibility or special viewport dimension issues.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,7 @@
 /* MODALS */
 /** general rules **/
-// When the user clicks on <span> (x), close the modal
-var close = document.getElementsByClassName("close")[0];
-var modal = document.getElementsByClassName("modal")[0];
-
-close.onclick = function() {
-    modal.style.display = "none";
-}
-
 // When the user clicks anywhere outside of the modal, close it
+var modal = document.getElementsByClassName("modal")[0];
 window.onclick = function(event) {
     if (event.target == modal) {
       modal.style.display = "none";
